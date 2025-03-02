076b0eaa6bfdc2345f45c8fd3c56a31af20b9f11
yashrana1974 <ranayash1974@gmail.com>
2025-03-02 14:21:16 +0530

    Added the update code in the database file

--- TkinterTutorial/database.py
+++ TkinterTutorial/database.py
@@ -10,7 +10,7 @@ root.title("Learn To Code at Codemy.com")
 #Image icon for tool icon
 root.iconbitmap("TkinterTutorial\\christmas-tree.ico")
 
-root.geometry("400x400")
+root.geometry("400x600")
 
 # Databases
 
@@ -33,6 +33,137 @@ c = conn.cursor()
 # )
           
 #           """)
+
+def saverecord():
+    # Create a databse or connct to one
+    conn = sqlite3.connect('address_book.db')
+
+    # Create cursor
+    # cursor is little thing that goes with query and fetchs data for use
+    c = conn.cursor()
+    
+    record_id = update_box.get()
+    c.execute("""UPDATE addressess SET 
+              first_name = :first,
+              last_name = :last,
+              address = :address,
+              city = :city,
+              state = :state,
+              zipcode = :zipcode
+              
+              WHERE oid = :oid
+              """,
+              {
+                  'first' : f_name_editor.get(),
+                  'last' : l_name_editor.get(),
+                  'address' : address_editor.get(),
+                  'city' : city_editor.get(),
+                  'state' : state_editor.get(),
+                  'zipcode' : zipcode_editor.get(),
+                  
+                  'oid' : record_id
+              }
+              )
+    
+    # Commit Changes
+    conn.commit()
+
+    # Close Connection
+    conn.close()
+    
+    editor.destroy()
+    
+
+# Create a fucntion to edit/update the record
+def update():
+    global editor
+    editor = Tk()
+    #Title for the window
+    editor.title("Update A Record")
+
+    #Image icon for tool icon
+    editor.iconbitmap("TkinterTutorial\\christmas-tree.ico")
+
+    editor.geometry("400x300")
+    
+    # Create a databse or connct to one
+    conn = sqlite3.connect('address_book.db')
+
+    # Create cursor
+    # cursor is little thing that goes with query and fetchs data for use
+    c = conn.cursor()
+    
+    record_id = update_box.get()
+    # Query the database (sqlite3 automatically create the primary key for the databse table so if we explicitly select it, it will be shown [oid])
+    c.execute("SELECT * FROM addressess WHERE oid = " + record_id)
+    # c.fetchmany(number)
+    # c.fetchone()
+    records = c.fetchall()
+    # print(records)
+
+    # Create Global variables for text box names
+    global f_name_editor
+    global l_name_editor
+    global address_editor
+    global city_editor
+    global state_editor
+    global zipcode_editor
+
+    # Create text boxes
+    f_name_editor = Entry(editor, width=30)
+    f_name_editor.grid(row=0, column=1, padx=20, pady=(10, 0))
+
+    l_name_editor = Entry(editor, width=30)
+    l_name_editor.grid(row=1, column=1, padx=20)
+
+    address_editor = Entry(editor, width=30)
+    address_editor.grid(row=2, column=1, padx=20)
+
+    city_editor = Entry(editor, width=30)
+    city_editor.grid(row=3, column=1, padx=20)
+
+    state_editor = Entry(editor, width=30)
+    state_editor.grid(row=4, column=1, padx=20)
+
+    zipcode_editor = Entry(editor, width=30)
+    zipcode_editor.grid(row=5, column=1, padx=20)
+
+    # Creaate Text Box Labels
+    f_name_label_editor = Label(editor, text="First Name")
+    f_name_label_editor.grid(row=0, column=0, pady=(10, 0)) #This will just add padding to the top only and not below it.
+
+    l_name_label_editor = Label(editor, text="Last Name")
+    l_name_label_editor.grid(row=1, column=0)
+
+    address_label_editor = Label(editor, text="Address")
+    address_label_editor.grid(row=2, column=0)
+
+    city_label_editor = Label(editor, text="City")
+    city_label_editor.grid(row=3, column=0)
+
+    state_label_editor = Label(editor, text="State")
+    state_label_editor.grid(row=4, column=0)
+
+    zipcode_label_editor = Label(editor, text="Zipcode")
+    zipcode_label_editor.grid(row=5, column=0)
+    
+    # for loop through the records
+    for record in records:
+        f_name_editor.insert(0, record[0])
+        l_name_editor.insert(0, record[1])
+        address_editor.insert(0, record[2])
+        city_editor.insert(0, record[3])
+        state_editor.insert(0, record[4])
+        zipcode_editor.insert(0, record[5])
+    
+    # Create a Update Button
+    updateBtn = Button(editor, text="Save Record", command=saverecord)
+    updateBtn.grid(row=6, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
+    
+    #creating a quit button
+    buttonQuit = Button(editor, text="Exit Program", command=editor.quit)
+    buttonQuit.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+    return
 
 # Create a function to delete the record
 def delete():
@@ -105,7 +236,7 @@ def query():
         print_records += str(record[0]) + " " + str(record[1]) + " " + "\t" + str(record[6]) + '\n'
     
     queryLabel = Label(root, text=print_records)
-    queryLabel.grid(row=12, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+    queryLabel.grid(row=14, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
     # Commit Changes
     conn.commit()
 
@@ -133,6 +264,9 @@ zipcode.grid(row=5, column=1, padx=20)
 delete_box = Entry(root, width=30)
 delete_box.grid(row=9, column=1)
 
+update_box = Entry(root, width=30)
+update_box.grid(row=11, column=1)
+
 # Creaate Text Box Labels
 f_name_label = Label(root, text="First Name")
 f_name_label.grid(row=0, column=0, pady=(10, 0)) #This will just add padding to the top only and not below it.
@@ -152,8 +286,11 @@ state_label.grid(row=4, column=0)
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
 
-delete_label = Label(root, text="ID Number")
+delete_label = Label(root, text="Delete ID")
 delete_label.grid(row=9, column=0)
+
+update_label = Label(root, text="Select ID")
+update_label.grid(row=11, column=0)
 
 # Create Submit button
 submit_btn = Button(root, text="Add Record To Database", command=submit)
@@ -167,6 +304,10 @@ queryBtn.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
 deleteBtn = Button(root, text="Delete Record", command=delete)
 deleteBtn.grid(row=10, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
 
+# Create a Update Button
+updateBtn = Button(root, text="Update Record", command=update)
+updateBtn.grid(row=12, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
+
 # Commit Changes
 conn.commit()
 
@@ -175,7 +316,7 @@ conn.close()
 
 #creating a quit button
 buttonQuit = Button(root, text="Exit Program", command=root.quit)
-buttonQuit.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+buttonQuit.grid(row=13, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
 
 #creating loop to continuosly execute the app
 root.mainloop()
