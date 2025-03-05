9e87270e016ffa341bcac98271f4809a58cc1630
yashrana1974 <ranayash1974@gmail.com>
2025-03-05 08:47:53 +0530

    added logo

---
+++ ScapyUI/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
+root = Tk()
+#Title for the window
+root.title("ScapyUI")
+
+#Image icon for tool icon
+root.iconbitmap("sem2_nfsuproject\\ScapyUI\\res\\logo.ico")
+
+
+#creating loop to continuosly execute the app
+root.mainloop()
